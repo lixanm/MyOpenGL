c722d0f924e39da5c2897002cb6c772524badc4e
lixanm <143246847+lixanm@users.noreply.github.com>
2025-08-15 20:45:44 +0800

    索引缓冲

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -127,30 +127,45 @@ int main(void)
     //顶点缓冲区
     float position[] = {
         -0.5f, -0.5f,  // 左下角
-         0.0f,  0.5f,  // 顶部
-         0.5f, -0.5f  // 右下角
+         0.5f, -0.5f,  // 顶部
+         0.5f,  0.5f,  // 右下角
+
+		-0.5f,  0.5f // 左上角
 	};
 
-	unsigned int buffer; //顶点缓冲区对象的ID
+    unsigned int indices[] = { //索引数组
+        0, 1, 2, // 第一个三角形
+        2, 3, 0  // 第二个三角形
+	};
 
+
+	unsigned int buffer; //顶点缓冲区对象的ID
     //生成缓冲区对象名称
 	glGenBuffers(1, &buffer); //生成一个顶点缓冲区并返回一个ID，参数1表示生成一个缓冲区，&buffer是存储生成的缓冲区ID的变量地址
-
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); //绑定顶点缓冲区对象（想要绑定缓冲区，只需绑定缓冲区ID即可）
-
     //缓冲区实际布局
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),position,GL_STATIC_DRAW); //分配缓冲区空间,将数据放入缓冲区中
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 *  sizeof(float),position,GL_STATIC_DRAW); //分配缓冲区空间,将数据放入缓冲区中
+
 
 
 	glEnableVertexAttribArray(0); //启用顶点属性数组，0表示顶点属性的位置（在着色器中定义）
-
     // 设置顶点属性指针
     //索引：0，这是顶点属性的位置，
     // 2表示每个顶点有两个分量（x和y），
     // GL_FLOAT表示数据类型，GL_FALSE表示不需要归一化，2 * sizeof(float)表示每个顶点的步长，(void*)0表示从缓冲区的起始位置开始读取数据
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void* const)0);
 
+
+
+	unsigned int ibo; //顶点缓冲区对象的ID
+	//生成缓冲区对象名称
+	glGenBuffers(1, &ibo); //生成一个顶点缓冲区并返回一个ID，参数1表示生成一个缓冲区，&buffer是存储生成的缓冲区ID的变量地址
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); //绑定顶点缓冲区对象（想要绑定缓冲区，只需绑定缓冲区ID即可）
+	//缓冲区实际布局
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW); //分配缓冲区空间,将数据放入缓冲区中
 	
+
+
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"/*, source.VertexSource, source.FragmentSource*/); //解析着色器文件，获取顶点着色器和片段着色器的源代码
 	
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource); //创建着色器程序
@@ -166,10 +181,16 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
 
+		//绘制6个顶点组成的三角形
 		//为缓冲区发出绘制调用，两个办法：glDrawArrays()(没有索引缓冲器) 或 glDrawElements()
-		glDrawArrays(GL_TRIANGLES, 0, 3); //绘制三角形，参数分别为绘制模式、起始索引、顶点数量
+		//glDrawArrays(GL_TRIANGLES, 0, 6); //绘制三角形，参数分别为绘制模式、起始索引、顶点数量
 
-        //告诉OpenGL顶点如何布局
+        //使用索引缓冲区绘制
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); //绘制三角形，参数分别为绘制模式、索引数量、索引类型、索引偏移量
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0); //绘制三角形，参数分别为绘制模式、索引数量、索引类型、索引偏移量
+
+
+
 
 
         /* Swap front and back buffers */
@@ -180,7 +201,7 @@ int main(void)
         glfwPollEvents();
     }
 
-    //glDeleteProgram(shader); //删除着色器程序
+    glDeleteProgram(shader); //删除着色器程序
 
     //清除GLFW资源
     glfwTerminate();
