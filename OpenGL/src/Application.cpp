c66e0f52e925308b0c7ed30c80607bd0f0b74c32
lixanm <143246847+lixanm@users.noreply.github.com>
2025-08-16 14:47:21 +0800

    整理代码

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,40 +3,59 @@
 
 #include <iostream>
 
-#include<fstream>
-#include <string>
-#include <sstream>
+#include<fstream>//文件流
+#include <string>//字符串
+#include <sstream>//字符串流
+
+#define ASSERT(x) if (!(x)) __debugbreak(); //断言宏，如果条件不成立，则触发异常
+
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x,__FILE__,__LINE__)); //宏定义，清除错误并检查错误
+
+static void GLClearError() //清除OpenGL错误
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) //检查OpenGL错误
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
+		return false;
+	}
+	return true; //返回true表示没有错误
+}
 
 struct ShaderProgramSource
 {
-    std::string VertexSource;
+	std::string VertexSource;
 	std::string FragmentSource;
 };
 
 static ShaderProgramSource ParseShader(const std::string& filepath)
 {
-    std::ifstream stream(filepath); //打开文件流
-    std::string line;
-    std::stringstream ss[2]; //创建两个字符串流，分别存储顶点着色器和片段着色器
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-    ShaderType type = ShaderType::NONE; //初始类型为NONE
-    while (getline(stream, line)) //逐行读取文件内容
-    {
-        if (line.find("#shader") != std::string::npos) //查找"#shader"关键字
-        {
-            if (line.find("vertex") != std::string::npos) //如果是顶点着色器
-                type = ShaderType::VERTEX;
-            else if (line.find("fragment") != std::string::npos) //如果是片段着色器
-                type = ShaderType::FRAGMENT;
-        }
-        else //如果不是"#shader"关键字
-        {
-            ss[(int)type] << line << "\n"; //将当前行添加到对应的字符串流中
-        }
-    }
+	std::ifstream stream(filepath); //打开文件流
+	std::string line;//行
+	std::stringstream ss[2]; //创建两个字符串流，分别存储顶点着色器和片段着色器
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};//枚举类型，表示着色器类型
+	ShaderType type = ShaderType::NONE; //初始类型为NONE
+	while (getline(stream, line)) //逐行读取文件内容
+	{
+		if (line.find("#shader") != std::string::npos) //查找"#shader"关键字
+		{
+			if (line.find("vertex") != std::string::npos) //如果是顶点着色器
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos) //如果是片段着色器
+				type = ShaderType::FRAGMENT;
+		}
+		else //如果不是"#shader"关键字
+		{
+			ss[(int)type] << line << "\n"; //将当前行添加到对应的字符串流中
+		}
+	}
 
 	return { ss[0].str(), ss[1].str() }; //返回顶点着色器和片段着色器的源代码
 }
@@ -45,46 +64,46 @@ static ShaderProgramSource ParseShader(const std::string& filepath)
 //先指定类型，再指定着色器源代码
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-    unsigned int id = glCreateShader(type);
+	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str(); //将std::string转换为const char*类型
 
 	glShaderSource(id, 1, &src, nullptr); //将着色器源代码附加到着色器对象上
-    glCompileShader(id);//编译着色器
+	glCompileShader(id);//编译着色器对象
 
-    //TODO:
-    int result;
+	//TODO:
+	int result;
 	glGetShaderiv(id, GL_COMPILE_STATUS, &result); //获取编译状态
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); //获取错误信息长度
-        char* message = (char*)alloca(length * sizeof(char)); //分配内存
-        glGetShaderInfoLog(id, length, &length, message); //获取错误信息
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER?"vertex":"fragment") << std::endl;
-        std::cout << message << std::endl; //输出错误信息
-        glDeleteShader(id); //删除着色器对象
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); //获取错误信息长度
+		char* message = (char*)alloca(length * sizeof(char)); //分配内存
+		glGetShaderInfoLog(id, length, &length, message); //获取错误信息
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER?"vertex":"fragment") << std::endl;
+		std::cout << message << std::endl; //输出错误信息
+		glDeleteShader(id); //删除着色器对象
 		return 0; //返回0表示编译失败
-    }
+	}
 
 
-    return id;
+	return id;
 }
 
 //静态，不会泄露到其他翻译单元
 //提供实际着色器源代码，链接定点着色器和片段着色器，返回标识符
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
-	unsigned int program = glCreateProgram(); //创建一个程序对象
+	unsigned int program = glCreateProgram(); //创建一个程序对象，程序对象是可以附加着色器对象的对象
 
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader); //创建顶点着色器对象,确定着色器类型为顶点着色器
 	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
-	glAttachShader(program, vs);
+	glAttachShader(program, vs);//将着色器对象附加到程序对象
 	glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-    
-	glDeleteShader(vs);
+	glLinkProgram(program);//成功链接程序对象
+	glValidateProgram(program);
+	
+	glDeleteShader(vs);//删除着色器
 	glDeleteShader(fs);
 
 	return program;
@@ -95,27 +114,25 @@ int main(void)
 {
 	GLFWwindow* window;//窗口指针
 
-    //初始化GLFW库
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	//初始化GLFW库
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    
+	
 	//创建一个窗口及其OpenGL上下文
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        //如果创建失败，终止GLFW并返回错误
-        glfwTerminate();
-        return -1;
-    }
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);/*监测（窗口模式），共享（不共享资源）*/
 
-    //将窗口的 OpenGL 上下文设为当前上下文
-    //需要创建一个有效的 OpenGL 渲染上下文并调用 glewInit() 初始化扩展入口点
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	if (!window)
+	{
+		//如果创建失败，终止GLFW并返回错误
+		glfwTerminate();
+		return -1;
+	}
 
-    //初始化GLEW
+	glfwMakeContextCurrent(window);//此函数使指定窗口的 OpenGL 或 OpenGL ES 上下文在调用线程上为当前上下文
+
+	//创建上下文后初始化GLEW
 	if(glewInit()!=GLEW_OK)
 		std::cout << "Error initializing GLEW" << std::endl;
 
@@ -123,87 +140,74 @@ int main(void)
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 
-    //定义一个顶点缓冲区，将数据发向显存，然后调用绘制函数
-    //顶点缓冲区
-    float position[] = {
-        -0.5f, -0.5f,  // 左下角
-         0.5f, -0.5f,  // 顶部
-         0.5f,  0.5f,  // 右下角
+	//顶点缓冲区
+	float position[] = {
+		-0.5f, -0.5f,  // 左下角
+		 0.5f, -0.5f,  // 顶部
+		 0.5f,  0.5f,  // 右下角
 
 		-0.5f,  0.5f // 左上角
 	};
-
-    unsigned int indices[] = { //索引数组
-        0, 1, 2, // 第一个三角形
-        2, 3, 0  // 第二个三角形
+	//索引缓冲区
+	unsigned int indices[] = { //索引数组
+		0, 1, 2, // 第一个三角形
+		2, 3, 0  // 第二个三角形
 	};
 
 
-	unsigned int buffer; //顶点缓冲区对象的ID
-    //生成缓冲区对象名称
-	glGenBuffers(1, &buffer); //生成一个顶点缓冲区并返回一个ID，参数1表示生成一个缓冲区，&buffer是存储生成的缓冲区ID的变量地址
-	glBindBuffer(GL_ARRAY_BUFFER, buffer); //绑定顶点缓冲区对象（想要绑定缓冲区，只需绑定缓冲区ID即可）
-    //缓冲区实际布局
-	glBufferData(GL_ARRAY_BUFFER, 6 * 2 *  sizeof(float),position,GL_STATIC_DRAW); //分配缓冲区空间,将数据放入缓冲区中
+	unsigned int buffer;//地址
+	glGenBuffers(1, &buffer);//在缓冲区中返回 n 个缓冲区对象名称，&buffer是指定存储生成的缓冲区对象名称的数组
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); //将缓冲区对象绑定到指定的缓冲区绑定点，即将顶点缓冲区对象绑定到GL_ARRAY_BUFFER绑定点，这样OpenGL就知道接下来要操作哪个缓冲区对象
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 *  sizeof(float),position,GL_STATIC_DRAW); 
+	//创建并初始化缓冲区对象的数据存储
+	//（指定目标缓冲区对象，指定缓冲区对象的新数据存储的大小，指定指向将复制到数据存储中进行初始化的数据的指针，指定数据存储的预期使用模式）
 
 
 
-	glEnableVertexAttribArray(0); //启用顶点属性数组，0表示顶点属性的位置（在着色器中定义）
-    // 设置顶点属性指针
-    //索引：0，这是顶点属性的位置，
-    // 2表示每个顶点有两个分量（x和y），
-    // GL_FLOAT表示数据类型，GL_FALSE表示不需要归一化，2 * sizeof(float)表示每个顶点的步长，(void*)0表示从缓冲区的起始位置开始读取数据
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void* const)0);
+	glEnableVertexAttribArray(0); //启用顶点属性数组，0表示通用顶点属性的索引
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void* const)0);
+	//定义通用顶点属性数据的数组
+	//（指定要修改的通用顶点属性的索引，
+	// 指定每个通用顶点属性的组件数，
+	// 指定数组中每个组件的数据类型，
+	// 指定连续通用顶点属性之间的字节偏移量，
+	// 指定当前绑定到 GL_ARRAY_BUFFER 目标的缓冲区的数据存储中数组中第一个泛型顶点属性的第一个组件的偏移量。初始值为 0。
+	// （(void*)0表示从缓冲区的起始位置开始读取数据））
 
 
-
-	unsigned int ibo; //顶点缓冲区对象的ID
-	//生成缓冲区对象名称
-	glGenBuffers(1, &ibo); //生成一个顶点缓冲区并返回一个ID，参数1表示生成一个缓冲区，&buffer是存储生成的缓冲区ID的变量地址
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); //绑定顶点缓冲区对象（想要绑定缓冲区，只需绑定缓冲区ID即可）
-	//缓冲区实际布局
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW); //分配缓冲区空间,将数据放入缓冲区中
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 	
 
 
-	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"/*, source.VertexSource, source.FragmentSource*/); //解析着色器文件，获取顶点着色器和片段着色器的源代码
-	
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader"); //解析着色器文件，获取顶点着色器和片段着色器的源代码
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource); //创建着色器程序
-	glUseProgram(shader); //使用着色器程序
+	glUseProgram(shader); //使用着色器程序，这些可执行文件成为当前状态的一部分
 
 
-    //主渲染循环
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        //每次循环开始清除颜色缓冲区
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	//主渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		//每次循环开始清除颜色缓冲区
+		glClear(GL_COLOR_BUFFER_BIT);
 
 
-		//绘制6个顶点组成的三角形
 		//为缓冲区发出绘制调用，两个办法：glDrawArrays()(没有索引缓冲器) 或 glDrawElements()
 		//glDrawArrays(GL_TRIANGLES, 0, 6); //绘制三角形，参数分别为绘制模式、起始索引、顶点数量
-
-        //使用索引缓冲区绘制
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); //绘制三角形，参数分别为绘制模式、索引数量、索引类型、索引偏移量
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0); //绘制三角形，参数分别为绘制模式、索引数量、索引类型、索引偏移量
+		//使用索引缓冲区绘制
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0)); //绘制三角形，参数分别为绘制模式、索引数量、索引类型、索引偏移量
 
 
 
 
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window); //避免画面撕裂
+		glfwSwapBuffers(window); //避免画面撕裂
 
-        //处理事件
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glDeleteProgram(shader); //删除着色器程序
-
-    //清除GLFW资源
-    glfwTerminate();
-    return 0;
+		glfwPollEvents();//处理事件
+	}
+	glDeleteProgram(shader); //删除着色器程序
+	glfwTerminate();//清除GLFW资源
+	return 0;
 }
